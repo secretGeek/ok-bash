51248cb3b001afc4713a35fc6821bab507c2a615
Doeke Zanstra <doeke@archipunt.nl>
2018-10-18 16:28:22 +0200

    Added show specific line number

--- ok-show.py
+++ ok-show.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import os, re, sys
+import argparse, os, re, sys
+
 
 def get_env(name, default):
     return os.environ[name] if name in os.environ else default
@@ -10,28 +11,27 @@ def get_env(name, default):
 def cprint(color, text=''):
     print(color+text, end='')
 
-re_heading = re.compile('^[ \t]*#')
-re_whitespace = re.compile('^[ \t]*$')
-re_comment = re.compile('(^[ \t]+)?(?<!\S)(?=#)(?!#\{)')
-# used for colored output
-c_nc      = '\033[0m'
-c_heading = get_env('OK_C_HEADING', '\033[0;31m')
-c_number  = get_env('OK_C_NUMBER',  '\033[0;36m')
-c_comment = get_env('OK_C_COMMENT', '\033[0;34m')
-c_command = get_env('OK_C_COMMAND', c_nc)
-c_prompt  = get_env('OK_C_PROMPT',  c_number)
-
-lines = sys.stdin.readlines()
-line_nr = 0
-nr_positions_line_nr = len(str(len(lines)))
-for line in lines:
+def print_line(line):
+    global line_nr
     if re_heading.search(line):
+        if args.only_line_nr is not None: 
+            return
         cprint(c_heading, line)
     elif re_whitespace.search(line):
+        if args.only_line_nr is not None: 
+            return
         cprint(c_nc, line)
     else:
         line_nr+=1
-        cprint(c_number, '{:{}}. '.format(line_nr, nr_positions_line_nr))
+        if args.only_line_nr is not None and args.only_line_nr != line_nr: 
+            return
+        if args.line_only:
+            print(line)
+            return
+        if args.only_line_nr is None:
+            cprint(c_number, '{:{}}. '.format(line_nr, nr_positions_line_nr))
+        else:
+            cprint(c_prompt, prompt)
         match = re_comment.search(line)
         if match:
             pos = match.start()
@@ -40,6 +40,32 @@ for line in lines:
         else:
             cprint(c_command, line)
     cprint(c_nc)
+    return
+
+re_heading = re.compile('^[ \t]*#')
+re_whitespace = re.compile('^[ \t]*$')
+re_comment = re.compile('(^[ \t]+)?(?<!\S)(?=#)(?!#\{)')
+# used for colored output
+c_nc      = '\033[0m'
+c_heading = get_env('_OK_C_HEADING', '\033[0;31m')
+c_number  = get_env('_OK_C_NUMBER',  '\033[0;36m')
+c_comment = get_env('_OK_C_COMMENT', '\033[0;34m')
+c_command = get_env('_OK_C_COMMAND', c_nc)
+c_prompt  = get_env('_OK_C_PROMPT',  c_number)
+# other customizations
+prompt    = get_env('_OK_PROMPT',  '$ ')
+verbose   = get_env('_OK_VERBOSE',  1)
+#arguments
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--line-only', action='store_true', default=False, help='Do not show prompt or colors with N')
+parser.add_argument('only_line_nr', metavar='N', type=int, nargs='?', help='the line number to show')
+args = parser.parse_args()
+
+lines = sys.stdin.readlines()
+line_nr = 0
+nr_positions_line_nr = len(str(len(lines)))
+for line in lines:
+    print_line(line)
 
 # http://www.apeth.com/nonblog/stories/textmatebundle.html
 # https://github.com/stedolan/jq/wiki/Docs-for-Oniguruma-Regular-Expressions-(RE.txt)

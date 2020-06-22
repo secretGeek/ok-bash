63d4b52ae0829565eaf46fd49800757a1d14b9cc
Doeke Zanstra <doekman@icloud.com>
2020-06-22 21:29:47 +0200

    Options (long form) also auto complete now

--- ok-complete.bash
+++ ok-complete.bash
@@ -1,16 +1,25 @@
 _ok_complete_bash() 
 {
-	local f cur prev opts
+	local double_options i prev_opts cur opts
 	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-
-	opts="$(ok --sys-cmds)" #commands
-
-	if [[ ${cur} != -* ]] ; then
-		COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-		return 0
+	double_options="^(-c|--comment-align|-f|--file|-a|--alias)$" # options that need an additional argument 
+	prev_opts=1 #1 if all previous words are options (start with a dash)
+	((i=1)) # first word is always "ok"
+	while [[ $i -lt ${COMP_CWORD} ]]; do
+		if [[ "${COMP_WORDS[$i]}" != -* ]]; then
+			prev_opts=0
+			break
+		fi
+		((i+=1))
+	done
+	if [[ $prev_opts == 1 ]]; then
+		cur="${COMP_WORDS[$COMP_CWORD]}"
+		prev="${COMP_WORDS[COMP_CWORD-1]}"
+		if [[ ! $prev =~ $double_options ]]; then
+			opts="$(ok --sys-opts) $(ok --sys-cmds)" #all options and commands, space seperated
+			COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+			return 0
+		fi
 	fi
 }
 complete -F _ok_complete_bash ok
-echo "ok-bash autocomplete initialized"
